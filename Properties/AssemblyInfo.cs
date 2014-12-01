1e810682745cd359faf7006d5866714503cd3001
Michael Yu <ItzWarty@gmail.com>
2014-12-01 15:40:53 -0800

    Update AssemblyInfo in preparation for NuGet release.
    Note: The totally isn't ready for prime time. Host-Guest proxying isn't implemented yet, testing is fairly minimal.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("libdsp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Dargon Service Protocol: Remote Procedure Call Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("The Dargon Project")]
 [assembly: AssemblyProduct("libdsp")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © ItzWarty 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
