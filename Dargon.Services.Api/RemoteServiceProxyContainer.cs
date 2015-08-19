436f364cc8519899cf9c98745c203bfece43d567
Warty <ItzWarty@gmail.com>
2015-08-19 03:05:01 -0700

    Services.Api exposes service getting with explicit service guid.

--- Dargon.Services.Api/RemoteServiceProxyContainer.cs
+++ Dargon.Services.Api/RemoteServiceProxyContainer.cs
@@ -1,5 +1,8 @@
-﻿namespace Dargon.Services {
+﻿using System;
+
+namespace Dargon.Services {
    public interface RemoteServiceProxyContainer {
       TService GetService<TService>() where TService : class;
+      TService GetService<TService>(Guid serviceGuid) where TService : class;
    }
 }
